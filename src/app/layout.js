f54e6abea939fd66e196de4b0cc5b0c0b3a1d92b
SeoJuAn <tjwndks333@naver.com>
2024-08-25 23:12:39 +0900

    change up

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'HanGPT Chatbot',
+  title: "SeoJuAn's AI Assistant",
   description: 'A powerful Chatbot Assistant',
 }
 
