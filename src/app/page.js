e1bcfce9d3956e575785c7e5859d8404e58b62e5
SeoJuAn <tjwndks333@naver.com>
2024-09-01 21:28:38 +0900

    str

--- src/app/page.js
+++ src/app/page.js
@@ -1331,7 +1331,7 @@ export default function Home() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ message: [input] }),
+        body: JSON.stringify({ message: input }),
       });
 
       if (!response.ok) {
